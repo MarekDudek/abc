b4d122d2620acecd8d12ac9bce7774374d9fc3cc
Marek Dudek <marek.dudek@gmail.com>
2018-06-08 00:29:05 +0200

    adding and removing nodes in Erdos

--- src/test/java/com/collibra/codingchallenge/ErdosGraphTest.java
+++ src/test/java/com/collibra/codingchallenge/ErdosGraphTest.java
@@ -3,30 +3,40 @@ package com.collibra.codingchallenge;
 import com.hendrix.erdos.graphs.AbstractGraph;
 import com.hendrix.erdos.graphs.SimpleDirectedGraph;
 import com.hendrix.erdos.interfaces.IData;
+import com.hendrix.erdos.types.Edge;
 import com.hendrix.erdos.types.IVertex;
 import com.hendrix.erdos.types.Vertex;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Objects;
+import java.util.Optional;
 
+import static com.hendrix.erdos.types.Edge.EDGE_DIRECTION.DIRECTED;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public final class ErdosGraphTest {
 
-    private static boolean exists(final AbstractGraph graph, final IData query) {
-        return graph.vertices().parallelStream().
-                reduce(
-                        false,
-                        (exists, vertex) -> exists || Objects.equals(vertex.getData(), query.getData()),
-                        (exists1, exists2) -> exists1 || exists2
-                );
-    }
-
     private static IVertex<String> vertex(final String id) {
         final IVertex<String> vertex = new Vertex<>();
         vertex.setData(id);
         return vertex;
+    }
+
+    private static Optional<IVertex> find(final AbstractGraph graph, final IData query) {
+        for (final IVertex vertex : graph.vertices())
+            if (Objects.equals(vertex.getData(), query.getData()))
+                return Optional.of(vertex);
+        return Optional.empty();
+    }
+
+    private static boolean exists(final AbstractGraph graph, final IData query) {
+        return find(graph, query).isPresent();
+    }
+
+    private static void removeVertex(final AbstractGraph graph, final IData query) {
+        find(graph, query).ifPresent(graph::removeVertex);
     }
 
     @Test
@@ -39,16 +49,31 @@ public final class ErdosGraphTest {
         graph.addVertex(vertex("node"));
         // then
         assertTrue(exists(graph, vertex("node")));
+        // when
+        removeVertex(graph, vertex("node"));
+        // then
+        assertFalse(exists(graph, vertex("node")));
     }
 
     @Test
+    @Ignore
     public void add_edge() {
+        // when
         final SimpleDirectedGraph graph = new SimpleDirectedGraph();
-        final Vertex<Object> node1 = new Vertex<>("node1");
-        final Vertex<Object> node2 = new Vertex<>("node2");
+        // then
+        // ...
+        // when
+        final IVertex<String> node1 = vertex("node1");
+        final IVertex<String> node2 = vertex("node2");
         graph.addVertex(node1);
         graph.addVertex(node2);
-        graph.addEdge(node1, node2);
-        graph.print();
+        final Edge edge = new Edge(vertex("node1"), vertex("node2"), DIRECTED, 23);
+        edge.setData("node1-node2-23");
+        graph.addEdge(edge);
+        // then
+        //graph.edges().parallelStream()
+        // when
+
+
     }
 }
