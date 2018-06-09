093a1a38b461ecfc4b47d2a8a22555c58aea53b0
Marek Dudek <marek.dudek@gmail.com>
2018-06-09 19:20:08 +0200

    adding and removing Jung edges

--- src/test/java/com/collibra/codingchallenge/graphs/JungGraphsTest.java
+++ src/test/java/com/collibra/codingchallenge/graphs/JungGraphsTest.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.junit.Test;
 
-import static com.collibra.codingchallenge.graphs.ErdosGraphs.node;
-import static com.collibra.codingchallenge.graphs.ErdosGraphs.removeNode;
+import static edu.uci.ics.jung.graph.util.EdgeType.DIRECTED;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -39,6 +38,54 @@ public final class JungGraphsTest {
         final boolean removedAgain = graph.removeVertex(new MyNode(id));
         // then
         assertThat(removedAgain, is(false));
+    }
+
+    @Test
+    public void adding_and_removing_edges() {
+        // given
+        final String from = "from";
+        final String to = "to";
+        final int weight = 10;
+        // when
+        final DirectedSparseMultigraph<MyNode, MyEdge> graph = new DirectedSparseMultigraph<>();
+        // then
+        assertThat(graph.containsEdge(new MyEdge(from, to, weight)), is(false));
+        assertThat(graph.containsEdge(new MyEdge(from, to, 23)), is(false)); // TODO
+        // when
+        graph.addVertex(new MyNode(from));
+        graph.addVertex(new MyNode(to));
+        final boolean added = graph.addEdge(new MyEdge(from, to, weight), new MyNode(from), new MyNode(to), DIRECTED);
+        // then
+        assertThat(added, is(true));
+        assertThat(graph.containsEdge(new MyEdge(from, to, weight)), is(true));
+        // when
+        final boolean addedAgain = graph.addEdge(new MyEdge(from, to, weight), new MyNode(from), new MyNode(to), DIRECTED);
+        // then
+        assertThat(addedAgain, is(false));
+        // when
+        final boolean removed = graph.removeEdge(new MyEdge(from, to, weight));
+        // then
+        assertThat(removed, is(true));
+        // when
+        final boolean removedAgain = graph.removeEdge(new MyEdge(from, to, weight));
+        // then
+        assertThat(removedAgain, is(false));
+        // when
+        final boolean addedYetAgain = graph.addEdge(new MyEdge(from, to, weight), new MyNode(from), new MyNode(to), DIRECTED);
+        // then
+        assertThat(addedYetAgain, is(true));
+        // when
+        final boolean fromRemoved = graph.removeVertex(new MyNode(from));
+        // then
+        assertThat(fromRemoved, is(true));
+        assertThat(graph.containsEdge(new MyEdge(from, to, weight)), is(false));
+        assertThat(graph.containsVertex(new MyNode(from)), is(false));
+        assertThat(graph.containsVertex(new MyNode(to)), is(true));
+        // when
+        final boolean toRemoved = graph.removeVertex(new MyNode(to));
+        // then
+        assertThat(toRemoved, is(true));
+        assertThat(graph.containsVertex(new MyNode(to)), is(false));
     }
 }
 
